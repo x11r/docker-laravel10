7fcfa849a14dd4f0b14d6b886ace97b72640a80c
Daisuke <zzzz.xo@gmail.com>
2023-06-01 12:55:26 +0900

    投稿者を表示

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -17,4 +17,9 @@ class Post extends Model
         'title',
         'content',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- src/resources/views/admin/posts/index.blade.php
+++ src/resources/views/admin/posts/index.blade.php
@@ -5,9 +5,15 @@
 @section('content')
     <div class="h1">投稿一覧</div>
     @foreach ($posts as $post)
-        <div class="p-2 border-b-8 bg-ligh">
-            <div>
-                {{ html()->a(route('admin.posts.edit',$post), $post->title)->class(['fw-bold', 'text-decoration-underline']) }}
+        <div class="p-2 m-2  bg-gray-100">
+            <div class="row">
+                <div class="col">
+                    {{ html()->a(route('admin.posts.edit',$post), $post->title)->class(['fw-bold', 'text-decoration-underline']) }}
+                </div>
+                <div class="col text-end">
+                    投稿者：{{ $post->user->name }}
+                    日付：{{ $post->created_at->format('Y-m-d H:i:s') }}
+                </div>
             </div>
             <div>{{ Str::limit($post->content, 100) }}</div>
         </div>
