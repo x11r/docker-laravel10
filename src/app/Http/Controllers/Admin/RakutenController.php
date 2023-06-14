79d0880e42a2913800bb63f00662cd3e6307691b
Daisuke <zzzz.xo@gmail.com>
2023-06-15 01:08:42 +0900

    楽天APIをキャッシュに入れる準備

--- src/app/Http/Controllers/Admin/RakutenController.php
+++ src/app/Http/Controllers/Admin/RakutenController.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Foundation\Bootstrap\HandleExceptions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class RakutenController extends Controller
 {
-    //
-
     public function __construct()
     {
         //
@@ -25,48 +26,37 @@ class RakutenController extends Controller
         $url = 'https://app.rakuten.co.jp/services/api/Travel/GetAreaClass/20131024?'
             . 'format=json&applicationId=' . $applicationId;
 
-        $response = Http::get($url);
-        $body = $response->body();
-//        $areas = json_decode($body, true);
+        // キャッシュから取得
+        $cacheKey = __METHOD__;
+        $cacheExpire = 60 * 60 * 24 * 3;
+        $jsonArray = Cache::remember($cacheKey, $cacheExpire, function() use ($url) {
+            $response = Http::get($url);
+            $body = $response->body();
+            $json = json_decode($body, true);
+            return $json;
+        });
 
         $params = [
-            'areas' => json_decode($body),
-            'areas_array' => json_decode($body, true),
+            'areas_array' => $jsonArray,
         ];
 
-        return view('admin.rakuten.area', $params);
+        return view('admin.rakuten.index', $params);
     }
 
-//    public function area()
-//    {
-//        dd(__LINE__);
-//    }
-
-    public function areaMiddle($middle)
+    public function areaMulti(string $middle, string $small, string $detail = '')
     {
-        $params = ['middle' => $middle];
-
-        $body = $this->getHotel($params);
-        $json = json_decode($body, true);
-
         $params = [
-            'hotels' => $json,
+            'middle' => $middle,
+            'small' => $small,
         ];
 
-        return view('admin.rakuten.hotel', $params);
-    }
+        if ($detail != '') {
+            $params['detail'] = $detail;
+        }
 
-    public function areaSmall($middle, $small)
-    {
-        $params = ['middle' => $middle, 'small' => $small];
-        $body = $this->getHotel($params);
-        $json = json_decode($body, true);
+        $hotels = $this->getHotels($params);
 
-        $params = [
-            'hotels' => $json,
-        ];
-
-        return view('admin.rakuten.hotel', $params);
+        return view('admin.rakuten.area', ['hotels' => $hotels]);
     }
 
     public function hotelDetail($hotelId)
@@ -74,28 +64,81 @@ class RakutenController extends Controller
 
         $applicationId = config('app.RAKUTEN_APPLICATION_ID');
 
+        $params = [];
+
         $url = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json'
             . '&applicationId=' . $applicationId
             . '&hotelNo=' . $hotelId;
 
-        $response = Http::get($url);
-        $body = $response->body();
+        Log::debug(__LINE__ . ' ' .  __METHOD__ . ' [url] ' . $url);
+        // キャッシュ設定
+        $cacheKey = __METHOD__ . ' ' . $hotelId;
+        $cacheExpire = 60 * 60 * 24 * 5;
 
-        $hotel = json_decode($body, true);
+        try {
 
-        $params = [
-            'hotel' => $hotel['hotels'][0],
-        ];
+            $response = Http::get($url);
+            $body = $response->body();
+            $json = json_decode($body, true);
+
+            $hotel = $json;
+
+            $params = [
+                'hotel' => $hotel,
+            ];
+
+        } catch (Exception|HandleExceptions $e) {
+            Log::debug(__LINE__ . ' ' . __METHOD__ . ' ' . $e->getMessage());
+
+            session()->flush();
+        }
+
+        dd($params['hotel']);
 
         return view('admin.rakuten.hotelDetail', $params);
     }
 
-    private function getHotel($params)
+    private function getHotels($params)
     {
         $applicationId = config('app.RAKUTEN_APPLICATION_ID');
 
         $url = 'https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&applicationId=' . $applicationId;
 
+        $url .= '&largeClassCode=japan';
+
+        if (! empty($params['middle'])) {
+            $url .= '&middleClassCode=' . $params['middle'];
+        }
+        if (! empty($params['small'])) {
+            $url .= '&smallClassCode=' . $params['small'];
+        }
+        if (! empty($params['detail'])) {
+            $url .= '&detailClassCode=' . $params['detail'];
+        }
+
+        $cacheKey = __METHOD__ . ' ' . $url;
+
+        if (Cache::has($cacheKey)) {
+            Log::debug(__LINE__ . ' ' . __METHOD__ . ' cache exists');
+        } else {
+            Log::debug(__LINE__ . ' ' . __METHOD__ . ' cache none');
+        }
+
+        $cacheExpire = 60 * 60 * 24 * 3;
+        $json = Cache::remember($cacheKey, $cacheExpire, function () use ($url) {
+            $response = Http::get($url);
+            $body = $response->body();
+            return json_decode($body, true);
+        });
+
+        return $json;
+    }
+
+    private function vacantHotel(array $params)
+    {
+        $applicationId = config('app.RAKUTEN_APPLICATION_ID');
+        $url = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?format=json';
+        $url .= '&applicationId=' . $applicationId;
         $url .= '&largeClassCode=japan';
 
         if (isset($params['middle'])) {
@@ -105,17 +148,43 @@ class RakutenController extends Controller
             $url .= '&smallClassCode=' . $params['small'];
         }
 
-//        try {
-        Log::debug(__LINE__ . ' ' . __METHOD__ . ' [url] ' . $url);
+        // キャッシュ設定
+        $cacheKey = __METHOD__ . ' ' . $url;
+        $cacheExpire = 60 * 60 * 24;
+        $json = Cache::remember($cacheKey, $cacheExpire, function () use ($url) {
             $response = Http::get($url);
-
-//        dd($response->status());
-
             $body = $response->body();
+            return json_decode($body, true);
+        });
 
-            return $body;
-//        } catch (Exception $e) {
+        if (isset($json->hotels)) {
+//            Log::debug(__LINE__);
+            foreach ($json->hotels as $hotel) {
+                Log::debug(__LINE__ . ' ' . print_r($hotel->hotel, true));
+                Log::debug(__LINE__
+                    . ' [hotelNo] ' . $hotel->hotel[0]->hotelBasicInfo->hotelNo
+                    . ' [hotelName] ' . $hotel->hotel[0]->hotelBasicInfo->hotelName
+                );
+//                Log::debug(__LINE__ . ' ' . __METHOD__ . ' [hotelNo] ' . $hotel->hotelBasicInfo->);
+            }
+        }
 
-//        }
+    }
+
+    public function hotelRanking()
+    {
+        $applicationId = config('app.RAKUTEN_APPLICATION_ID');
+        $url = 'https://app.rakuten.co.jp/services/api/Travel/HotelRanking/20170426?format=json';
+        $url .= '&applicationId=' . $applicationId;
+
+        $cacheKey = __METHOD__ . ' ' . $url;
+        $cacheExpire = 60 * 60 * 24;
+
+        $json = Cache::remember($cacheKey, $cacheExpire, function () use ($url) {
+            Log::debug(__LINE__ . ' ' . __METHOD__ . ' ' . $url);
+            $response = Http::get($url);
+            $body = $response->body();
+            return json_decode($body);
+        });
     }
 }
--- src/resources/views/admin/rakuten/area.blade.php
+++ src/resources/views/admin/rakuten/area.blade.php
@@ -3,26 +3,17 @@
 @section('title', '楽天')
 
 @section('content')
-
     <div class="container">
         <div class="h1">楽天API</div>
-        @foreach ($areas_array['areaClasses']['largeClasses'] as $area)
-            @foreach ($area['largeClass'][1]['middleClasses'] as $middle)
-                <div class="row">
-                    <div class="col">
-                        {{ $middle['middleClass'][0]['middleClassName'] }}
-                    </div>
-                    @foreach ($middle['middleClass'][1]['smallClasses'] as $small)
-                        <div class="col">
-                            <a
-                                href="{{ route('admin.rakuten.area-small',
-                                    ['middle' => $middle['middleClass'][0]['middleClassCode'], 'small' => $small['smallClass'][0]['smallClassCode']]) }}">                           {{ $small['smallClass'][0]['smallClassName'] }}
-                            </a>
-                        </div>
-                    @endforeach
+        @foreach ($hotels['hotels'] as $hotel)
+            <div class="row">
+                <div class="h4">
+                    {{ $hotel['hotel'][0]['hotelBasicInfo']['hotelName'] }}
                 </div>
-            @endforeach
+                <div class="">
+                    {{ $hotel['hotel'][0]['hotelBasicInfo']['hotelNo'] }}
+                </div>
+            </div>
         @endforeach
-
     </div>
 @endsection
