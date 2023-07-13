25aefee45c87cc1ae2542db462d19c69e9c9a9ae
Hirano Daisuke <zzzz.xo@gmail.com>
2023-07-13 16:57:45 +0900

    レイアウト変更

--- src/resources/views/rakuten/hotelDetail.blade.php
+++ src/resources/views/rakuten/hotelDetail.blade.php
@@ -1,15 +1,17 @@
-@extends('layouts.admin')
+@extends('layouts.rakuten')
 
-@section('title', '楽天')
+@section('title', '楽天トラベルAPI ホテル詳細')
 
 @section('content')
     <div class="container">
-        <div class="h1"><a href="{{ route('rakuten.areas') }}">楽天API</a></div>
-        <div class="h1">{{ $hotels[0]->hotel[0]->hotelBasicInfo->hotelName }}</div>
-        <div class="bg-amber-200">
+        <div class="h1 bg-red-100 p-2">
+            <a href="{{ route('rakuten.areas') }}">楽天トラベルAPI</a> ホテル詳細
+        </div>
+        <div class="h2">{{ $hotels[0]->hotel[0]->hotelBasicInfo->hotelName }}</div>
+        <div class="bg-red-100 m-1">
             @foreach ($hotels[0]->hotel as $hotelKey => $hotelDetail)
                 @if ($hotelKey === 0)
-                    <div class="bg-amber-100 p-2">
+                    <div class="bg-white m-1">
                         <div>
                             {{ $hotelDetail->hotelBasicInfo->hotelName }}
                             <span class="small">({{ $hotelDetail->hotelBasicInfo->hotelKanaName }})</span>
@@ -50,38 +52,43 @@
                         <div>{{ $hotelDetail->hotelBasicInfo->parkingInformation }}</div>
                         <div>最寄り駅：{{ $hotelDetail->hotelBasicInfo->nearestStation }}</div>
                         <div><img src="{{ $hotelDetail->hotelBasicInfo->hotelImageUrl }}" alt=""></div>
-                        <div><img src="{{ $hotelDetail->hotelBasicInfo->hotelThumbnailUrl }}" alt=""></div>
+{{--                            <div><img src="{{ $hotelDetail->hotelBasicInfo->hotelThumbnailUrl }}" alt=""></div>--}}
                         <div><img src="{{ $hotelDetail->hotelBasicInfo->roomImageUrl }}" alt=""></div>
-                        <div><img src="{{ $hotelDetail->hotelBasicInfo->roomThumbnailUrl }}" alt=""></div>
+{{--                            <div><img src="{{ $hotelDetail->hotelBasicInfo->roomThumbnailUrl }}" alt=""></div>--}}
                         <div><img src="{{ $hotelDetail->hotelBasicInfo->hotelMapImageUrl }}" alt=""></div>
                         <div>レビュー数：{{ $hotelDetail->hotelBasicInfo->reviewCount }}</div>
                         <div>平均レビュー：{{ $hotelDetail->hotelBasicInfo->reviewAverage }}</div>
                         <div>ユーザーレビュー：{{ $hotelDetail->hotelBasicInfo->userReview }}</div>
                     </div>
                 @else
-                    <div class="bg-amber-100 m-3">
+                    <div class="bg-white m-2">
+                        @php
+                            $roomBasicInfo = $hotelDetail->roomInfo[0]->roomBasicInfo;
+							$dailyCharge = $hotelDetail->roomInfo[1]->dailyCharge;
+                        @endphp
 
-                        <div>部屋クラス = {{ $hotelDetail->roomInfo[0]->roomBasicInfo->roomClass }}</div>
-                        <div>部屋名：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->roomName }}</div>
-                        <div>プランID：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->planId }}</div>
-                        <div>プラン名：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->planName }}</div>
-                        <div>ポイントレート；{{ $hotelDetail->roomInfo[0]->roomBasicInfo->pointRate }}</div>
-                        <div>夕飯付きフラグ：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->withDinnerFlag }}</div>
-                        <div>夕飯選択フラグ：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->dinnerSelectFlag }}</div>
-                        <div>朝食付きフラグ：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->withBreakfastFlag }}</div>
-                        <div>朝食選択フラグ：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->breakfastSelectFlag }}</div>
-                        <div>支払い：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->payment }}</div>
-                        <div>予約URL：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->reserveUrl }}</div>
-                        <div>営業フォームURL：{{ $hotelDetail->roomInfo[0]->roomBasicInfo->salesformFlag }}</div>
+                        <div>部屋クラス = {{ $roomBasicInfo->roomClass }}</div>
+                        <div>部屋名：{{ $roomBasicInfo->roomName }}</div>
+                        <div>プランID：{{ $roomBasicInfo->planId }}</div>
+                        <div>プラン名：{{ $roomBasicInfo->planName }}</div>
+                        <div>ポイントレート；{{ $roomBasicInfo->pointRate }}</div>
+                        <div>夕飯付きフラグ：{{ $roomBasicInfo->withDinnerFlag }}</div>
+                        <div>夕飯選択フラグ：{{ $roomBasicInfo->dinnerSelectFlag }}</div>
+                        <div>朝食付きフラグ：{{ $roomBasicInfo->withBreakfastFlag }}</div>
+                        <div>朝食選択フラグ：{{ $roomBasicInfo->breakfastSelectFlag }}</div>
+                        <div>支払い：{{ $roomBasicInfo->payment }}</div>
+                        <div>予約URL：{{ $roomBasicInfo->reserveUrl }}</div>
+                        <div>営業フォームURL：{{ $roomBasicInfo->salesformFlag }}</div>
 
-                        <div>滞在日：{{ $hotelDetail->roomInfo[1]->dailyCharge->stayDate }}</div>
-                        <div>楽天チャージ：{{ $hotelDetail->roomInfo[1]->dailyCharge->stayDate }}</div>
-                        <div>合計：{{ $hotelDetail->roomInfo[1]->dailyCharge->total }}</div>
-                        <div>チャージフラグ：{{ $hotelDetail->roomInfo[1]->dailyCharge->chargeFlag }}</div>
+                        <div>滞在日：{{ $dailyCharge->stayDate }}</div>
+                        <div>楽天チャージ：{{ $dailyCharge->stayDate }}</div>
+                        <div>合計：{{ $dailyCharge->total }}</div>
+                        <div>チャージフラグ：{{ $dailyCharge->chargeFlag }}</div>
 
                     </div>
                 @endif
             @endforeach
+
         </div>
     </div>
 @endsection
