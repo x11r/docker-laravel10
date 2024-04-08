eaf6ff934c9db851af058c13d5c25e6e6ad61d82
Daisuke <zzzz.xo@gmail.com>
2024-04-08 19:21:10 +0900

    楽天API向け修正

---
+++ src/resources/views/rakuten/react.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <title>reactjs api rakuten</title>
+    <meta name="viewport" content="device-width, initial-scale=1, shrink0to-fit=no">
+    @vite(['resources/js/rakutenReact.js'])
+</head>
+<body>
+<div id="app">
+</div>
+</body>
+</html>
